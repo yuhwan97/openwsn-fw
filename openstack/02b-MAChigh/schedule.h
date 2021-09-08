1db52fe9a21ee4d20a6f2bfb21a56a387f4133d4
yuhwan97 <yuhwan9706@gmail.com>
2021-09-08 15:32:15 +0900

    merge

--- openstack/02b-MAChigh/schedule.h
+++ openstack/02b-MAChigh/schedule.h
@@ -17,7 +17,7 @@
 
 The superframe reappears over time and can be arbitrarily long.
 */
-#define SLOTFRAME_LENGTH    20 //should be 101
+#define SLOTFRAME_LENGTH    50 //should be 101
 
 //draft-ietf-6tisch-minimal-06
 #define SCHEDULE_MINIMAL_6TISCH_ACTIVE_CELLS                      1
@@ -37,7 +37,7 @@ The superframe reappears over time and can be arbitrarily long.
   for serial port to transmit data to dagroot.
 */
 
-#define NUMSLOTSOFF          20
+#define NUMSLOTSOFF          30
 
 /**
 \brief Maximum number of active slots in a superframe.
